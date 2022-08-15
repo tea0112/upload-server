d06116494592362608289596159374d2332b33f9
tea0112 <ducthaidev@gmail.com>
2022-08-15 17:54:07 +0700

    refactor: add new dependencies

--- route/routes.go
+++ route/routes.go
@@ -3,11 +3,16 @@ package route
 import (
 	"context"
 	"net/http"
+
+	helper "github.com/tea0112/upload-server/helpers"
+	"github.com/tea0112/upload-server/aws"
 	"github.com/tea0112/upload-server/user"
 )
 
 func NewRoute(mux *http.ServeMux, ctx context.Context) *http.ServeMux {
-	userController := user.NewUserControllerImpl(ctx)
+	fileWriter := helper.Writing{}
+	aws := aws.AwsImpl{}
+	userController := user.NewUserControllerImpl(ctx, fileWriter, aws)
 
 	mux.HandleFunc("/user/batch", userController.Upload)
 
