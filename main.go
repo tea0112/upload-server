bb7628cca88d98cf2233623d2b8304120a5530dd
tea0112 <ducthaidev@gmail.com>
2022-08-15 17:51:23 +0700

    refactor: remove unnecessary variables

--- main.go
+++ main.go
@@ -16,10 +16,6 @@ const (
 func main() {
 	ctx := context.Background()
 
-	session := server.NewAWSS3(AWS_S3_REGION, AWS_S3_BUCKET)
-	ctx = context.WithValue(ctx, "s3_session", session)
-	ctx = context.WithValue(ctx, "s3_bucket", AWS_S3_BUCKET)
-
 	server := server.NewServer(ctx)
 	server.SetPort(PORT)
 	server.Run()
