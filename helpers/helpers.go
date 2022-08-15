1c39fa3d1b8ee5eba6a9633a4a8c4ab89315ca81
tea0112 <ducthaidev@gmail.com>
2022-08-15 17:53:20 +0700

    feat: add file writer helper

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,9 +1,31 @@
 package helper
 
+import "os"
+
 func IsEmpty(str string) bool {
 	if str == "" {
 		return true
 	} else {
 		return false
 	}
+}
+
+type FileWriter interface {
+	WriteFile(string, []byte) error
+}
+
+type Writing struct{}
+
+func (w Writing) WriteFile(path string, buffer []byte) error {
+	f, err := os.Create("/tmp/file")
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(buffer)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
