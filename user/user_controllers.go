ad301c298b5c159e9a84b6c1a545d48d2c1b5465
tea0112 <ducthaidev@gmail.com>
2022-08-15 17:49:29 +0700

    refector: change max byte upload, add aws uploading function, add write file to tmp, fix dependency injection

--- user/user_controllers.go
+++ user/user_controllers.go
@@ -7,31 +7,36 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/tea0112/upload-server/errors"
+	"github.com/tea0112/upload-server/aws"
+	errs "github.com/tea0112/upload-server/errors"
+	helper "github.com/tea0112/upload-server/helpers"
 )
 
-const maxByteRead = 1146
+const maxByteRead = 1300
 
 type UserController interface {
 	Upload(w http.ResponseWriter, r *http.Request)
 }
 
 type UserControllerImpl struct {
-	context context.Context
+	context    context.Context
+	fileWriter helper.FileWriter
+	aws        aws.Aws
 }
 
-func NewUserControllerImpl(ctx context.Context) UserController {
-	return UserControllerImpl{context: ctx}
+func NewUserControllerImpl(ctx context.Context, fileWriter helper.FileWriter, aws aws.Aws) UserController {
+	return UserControllerImpl{context: ctx, fileWriter: fileWriter, aws: aws}
 }
 
 func (u UserControllerImpl) Upload(w http.ResponseWriter, r *http.Request) {
+
 	buffer := make([]byte, maxByteRead)
-	counter := 0	
+	counter := 0
 
 	for {
 		n, err := r.Body.Read(buffer)
 		counter += n
-		if counter >= maxByteRead {
+		if counter > maxByteRead {
 			http.Error(w, errs.ErrExceedBodySize.Error(), http.StatusBadRequest)
 			return
 		}
@@ -43,14 +48,28 @@ func (u UserControllerImpl) Upload(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	jsonBuffer := buffer[:counter]
 	var jsonBody map[string]interface{}
 
-	err := json.Unmarshal(buffer[:counter], &jsonBody)
+	// write to tmp directory for preparing upload
+	fileDir := "/tmp/file"
+	err := u.fileWriter.WriteFile(fileDir, jsonBuffer)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
 
+	err = u.aws.UploadFile(fileDir)
+	if err != nil {
+		http.Error(w, errs.ErrUploading.Error(), http.StatusBadRequest)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(jsonBody)
 	w.WriteHeader(http.StatusOK)
+
+	err = json.Unmarshal(jsonBuffer, &jsonBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	json.NewEncoder(w).Encode(jsonBody)
 }
